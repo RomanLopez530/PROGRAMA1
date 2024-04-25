4d6afbc8bc82b9baf7d4b31eaaff965a76277012
RomanLopez530 <romansaidlopezriv@gmail.com>
2024-04-25 09:10:06 -0600

    :l

--- tarea-1-de-abril/taea-1-de-abril.cpp
+++ tarea-1-de-abril/taea-1-de-abril.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
+int factorial(int n)
+{
+	int factorial = 1;
+	for (int i = 1; i <= n; ++i) {
+		factorial *= i;
+	}
+	return factorial;
+}
 int main(int argc, char *argv[]) {
 	int n1 = 0, n2= 1 , f, n;
 	cout<<"a que numero quiere que llegue";
